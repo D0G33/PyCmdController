f70e3c5b2c5240a50d2ad8bc0550fa40df718643
D0G33 <brysonrogersmith@gmail.com>
2024-07-30 23:24:18 -0500

    -Updated _cmd to increase command execution time
    -Added Vaugely legal main script
    
    -Changed default to set window to invisible instead of hiding to skip the minimizing animation.

--- PyCmdController.py
+++ PyCmdController.py
@@ -22,7 +22,6 @@ class cmdObject:
         while True:
             try:
                 window_handle = win32gui.FindWindow(None, name)
-
                 if window_handle != 0:
                     if laziness != 0: sleep(1 / (1 + (laziness / 100)))
                     return window_handle
@@ -50,12 +49,17 @@ class cmdObject:
         # Activate Window and Allow Keyboard Input
         win32gui.ShowWindow(self.window_handle, win32con.SW_SHOW)
         win32gui.BringWindowToTop(self.window_handle)
+
         # Click the window for safety
-        x, y, w, h = self.getRect()
-        pyautogui.click(x + 50, y + 50)
+        _x, _y, _w, _h = self.getRect()
+        pyautogui.click(_x + 50, _y + 50)
 
     def hide(self):
+
         win32gui.ShowWindow(self.window_handle, win32con.SW_MINIMIZE)
+
+    def show(self):
+        win32gui.ShowWindow(self.window_handle, win32con.SW_SHOW)
 
     def invisible(self):
         #Not Recommended for Debugging Purposes
@@ -63,13 +67,19 @@ class cmdObject:
 
     def _cmd(self,cmd):
         #Intended for internal use only
-        pyautogui.write(cmd, interval=0)
-        pyautogui.press("enter")
+        pyautogui.write(cmd + "\n", interval=0)
 
     def cmd(self,*cmd):
         self.activate()
-        for i in cmd:
-            self._cmd(i)
+        if cmd[0] != None:
+            for i in cmd:
+                self._cmd(i)
+        else:
+            _list = list(cmd)
+            _list.pop(0)
+            for i in _list:
+                self._cmd(i)
+        self.invisible()
 
     def close(self):
         win32gui.PostMessage(self.window_handle, win32con.WM_CLOSE, 0, 0)
@@ -77,13 +87,41 @@ class cmdObject:
         self.cmd("runas /user:"+user+" cmd", password)
         self.close()
         self.window_handle = self.collectHandle("cmd "+"(running as "+domain+"\\"+user+")")
-        self.hide()
+        self.cmd("title "+self.name)
+        self.invisible()
 
 def main():
     user,password = 'pythonAdmin', 'PythonPassword'
     testObj = cmdObject("the correct window name")
     testObj.elevate(user,password)
-
+    testObj.invisible()
+    testObj.activate()
+    testObj.cmd(None,
+                "echo Hacking into the white house.",
+                "cls",
+                "echo Hacking into the white house..",
+                "cls",
+                "echo Hacking into the white house...",
+                "cls",
+                "echo Hacking into the white house.",
+                "cls",
+                "echo Hacking into the white house..",
+                "cls",
+                "echo Hacking into the white house...",
+                "cls",
+                "echo Hacking into the white house.",
+                "cls",
+                "echo Hacking into the white house..",
+                "cls",
+                "echo Hacking into the white house...",
+                "cls",
+                "echo Hacking into the white house.",
+                "cls",
+                "echo Hacking into the white house..",
+                "cls",
+                "echo Hacking into the white house..."
+                )
+    testObj.show()
 
 if __name__ == '__main__':
     main()
